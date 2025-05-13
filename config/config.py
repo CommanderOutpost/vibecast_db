64d58c38f44c35ae680d350c2ad0615b503c936f
CommanderOutpost <amiolas58@gmail.com>
2025-05-13 14:10:05 +0100

    removes default settings

--- config/config.py
+++ config/config.py
@@ -3,16 +3,16 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # MongoDB config for chat/presence
-    MONGO_URI: str = "mongodb://localhost:27017"
-    MONGO_DB: str = "zance1"
+    MONGO_URI: str
+    MONGO_DB: str
 
     # Redis config for presence tracking
-    REDIS_HOST: str = "localhost"
-    REDIS_PORT: int = 6379
-    REDIS_DB: int = 1  # separate DB from your OTP microservice
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DB: int
 
     # JWT
-    JWT_SECRET_KEY: str = "YOUR_SUPER_SECRET_KEY"
+    JWT_SECRET_KEY: str
 
     class Config:
         env_file = ".env"
